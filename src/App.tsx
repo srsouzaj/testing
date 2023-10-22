d0c2589fc6b60797f02556394886b9d1c2df772c
Jorge de Souza <jorgejrdj1@gmail.com>
2023-10-22 19:48:10 -0300

    feat: initial test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState<string>(
+    "Let's learn more about testing in React")
+
   return (
     <div>
       <h1>Hello world!</h1>
-      <p>Let's learn more about testing in React</p>
+      <p>{message}</p>
+      <button onClick={() => setMessage("New message")}>Change message</button>
     </div>
   );
 }
